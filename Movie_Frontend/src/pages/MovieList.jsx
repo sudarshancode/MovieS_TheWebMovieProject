072edfeb261ef86f5c50a6811e99f0021c97d33b
sudarshan chakraborty <sudarshanchakraborty420@gmail.com>
2026-01-02 23:54:36 +0530

    Add Load Button

--- Movie_Frontend/src/pages/MovieList.jsx
+++ Movie_Frontend/src/pages/MovieList.jsx
@@ -1,28 +1,37 @@
 import { useEffect, useState } from "react";
 import MovieCard from "../component/MovieCard";
 
-function MovieList(){
-    const [movies,setMovies]=useState([]);
+function MovieList() {
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [page,setPage]=useState(1);
 
-    useEffect(()=>{
-        fetch('https://jsonfakery.com/movies/simple-paginate')
-        .then((res)=>{
-            return res.json();
-        })
-        .then((data)=>{
-            console.log("API RESPONSE 👉", data);
-            setMovies(data.data);
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
-    },[]);
+    useEffect(() => {
+        fetch(`https://jsonfakery.com/movies/simple-paginate?page=${page}`)
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                setMovies(prev=>[...prev,...data.data]);
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.log(err);
+                setLoading(false);
+            })
+    }, [page]);
+    if (loading) {
+        return <p style={{ textAlign: "center", marginTop: "50px" }}>Loading Movies......</p>
+    }
     return (
-       <div className="movie-container">
-        {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
-       </div> 
+        <div className="movie-container">
+            {movies.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+            ))}
+            <div className="load-more-wrapper">
+                <button onClick={() => setPage(page+1)}>Load More</button>
+            </div>
+        </div>
     )
 }
 export default MovieList;
